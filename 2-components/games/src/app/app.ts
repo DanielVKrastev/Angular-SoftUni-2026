f10a8cd4a629a78879859989d3c0ae638be4e322
Daniel Krastev <danielkrastev01@gmail.com>
2026-03-09 22:12:44 +0200

    Create About Component

--- 2-components/games/src/app/app.ts
+++ 2-components/games/src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { About } from './about/about';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, About],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
 }

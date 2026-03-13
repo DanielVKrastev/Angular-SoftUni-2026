227de792cb4ccb162ccd78f7297b33879a9fa648
Daniel Krastev <danielkrastev01@gmail.com>
2026-03-13 23:00:40 +0200

    Add user

--- 3.CD,SOLID,Services/demo-app/src/app/app.component.ts
+++ 3.CD,SOLID,Services/demo-app/src/app/app.component.ts
@@ -13,12 +13,28 @@ import { SimpleUser } from './types';
 export class AppComponent {
   title = 'demo-app';
   appUsers: SimpleUser[] = [
-    {name: 'Daniel', age: 12},
-    {name: 'Anna', age: 24},
-    {name: 'Maria', age: 34},
+    { name: 'Daniel', age: 12 },
+    { name: 'Anna', age: 24 },
+    { name: 'Maria', age: 34 },
   ]
 
   handleClick() {
     this.title = 'ZDR';
+  }
+
+  addUser(inputName: HTMLInputElement, inputAge: HTMLInputElement) {
+    // console.log(inputName.value);
+    // console.log(inputAge.value);
+
+    const user = { 
+      name: inputName.value, 
+      age: Number(inputAge.value) 
+    }
+    this.appUsers.push(user);
+    inputName.value = '';
+    inputAge.value = '';
+
+    console.log(this.appUsers);
+    
   }
 }

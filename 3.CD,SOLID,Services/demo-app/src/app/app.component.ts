4a5c04d4f2b162aff649b3ac6dbf5d249bf30c54
Daniel Krastev <danielkrastev01@gmail.com>
2026-03-14 08:15:04 +0200

    Create User Service and use in App component

--- 3.CD,SOLID,Services/demo-app/src/app/app.component.ts
+++ 3.CD,SOLID,Services/demo-app/src/app/app.component.ts
@@ -2,40 +2,32 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserListComponent } from './user-list/user-list.component';
 import { SimpleUser } from './types';
+import { UserService } from './user-list/user.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, UserListComponent],
+  providers: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'demo-app';
-  appUsers: SimpleUser[] = [
-    { name: 'Daniel', age: 12 },
-    { name: 'Anna', age: 24 },
-    { name: 'Maria', age: 34 },
-  ]
+
+  users: SimpleUser[] = [];
+
+  constructor(private userService: UserService) {
+    this.users = userService.appUsers;
+  }
 
   handleClick() {
     this.title = 'ZDR';
   }
 
-  addUser(inputName: HTMLInputElement, inputAge: HTMLInputElement) {
-    // console.log(inputName.value);
-    // console.log(inputAge.value);
-
-    const user = { 
-      name: inputName.value, 
-      age: Number(inputAge.value) 
-    }
-    this.appUsers.push(user);
-    //this.appUsers = [...this.appUsers, user]; //!Important hack
-    inputName.value = '';
-    inputAge.value = '';
-
-    console.log(this.appUsers);
-    
+  addSimpleUser(inputName: HTMLInputElement,
+    inputAge: HTMLInputElement){
+      // validate input
+    this.userService.addUser(inputName, inputAge)
   }
 }
---
+++ 3.CD,SOLID,Services/demo-app/src/app/user-list/user.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { SimpleUser } from '../types';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  appUsers: SimpleUser[] = [
+    { name: 'Daniel', age: 12 },
+    { name: 'Anna', age: 24 },
+    { name: 'Maria', age: 34 },
+  ]
+  constructor() { }
+
+   addUser(inputName: HTMLInputElement, inputAge: HTMLInputElement) {
+    // console.log(inputName.value);
+    // console.log(inputAge.value);
+
+    const user = { 
+      name: inputName.value, 
+      age: Number(inputAge.value) 
+    }
+    this.appUsers.push(user);
+    //this.appUsers = [...this.appUsers, user]; //!Important hack
+    inputName.value = '';
+    inputAge.value = '';
+
+    console.log(this.appUsers);
+    
+  }
+}

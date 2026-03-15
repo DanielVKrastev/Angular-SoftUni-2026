f6d1ab1d07e541c1932393469c666025cb920743
Daniel Krastev <danielkrastev01@gmail.com>
2026-03-15 15:04:40 +0200

    Create UI

---
+++ 4.Workshop-part-1/workshop/src/app/posts-list/posts-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-posts-list',
+  imports: [],
+  templateUrl: './posts-list.html',
+  styleUrl: './posts-list.css',
+})
+export class PostsList {}
---
+++ 4.Workshop-part-1/workshop/src/app/themes-list/themes-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-themes-list',
+  imports: [],
+  templateUrl: './themes-list.html',
+  styleUrl: './themes-list.css',
+})
+export class ThemesList {}

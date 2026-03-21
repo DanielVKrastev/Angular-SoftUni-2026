fabaaca3b9e99770a80d208a3f9751838961d656
Daniel Krastev <danielkrastev01@gmail.com>
2026-03-21 20:17:46 +0200

    Create API Service and get data

--- 4.Workshop-part-1/workshop/src/app/app.ts
+++ 4.Workshop-part-1/workshop/src/app/app.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './core/header/header';
@@ -14,17 +13,4 @@ import { MainComponent } from './main/main.component';
 export class App{
   title = 'workshop';
 
-  /*
-  constructor(private http: HttpClient){}
-
-  ngOnInit(): void {
-    this.http.get(
-      'https://workshop-angular-feb-2026-default-rtdb.europe-west1.firebasedatabase.app/product.json'
-    )
-    .subscribe((x) => {
-      console.log(x);
-      
-    })
-  }
-    */
 }
--- 4.Workshop-part-1/workshop/src/app/posts-list/posts-list.ts
+++ 4.Workshop-part-1/workshop/src/app/posts-list/posts-list.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../apiService';
 
 @Component({
   selector: 'app-posts-list',
@@ -6,4 +7,15 @@ import { Component } from '@angular/core';
   templateUrl: './posts-list.html',
   styleUrl: './posts-list.css',
 })
-export class PostsList {}
+export class PostsList implements OnInit {
+  constructor(private apiService: ApiService) {
+
+  }
+
+  ngOnInit(): void {
+    this.apiService.getPosts().subscribe(p => {
+      console.log(p);
+      
+    })
+  }
+}

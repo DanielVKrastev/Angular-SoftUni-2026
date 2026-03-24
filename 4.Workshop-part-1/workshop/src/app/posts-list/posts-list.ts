ce664f5d2ecba654c2193013243efc3e63aef42e
Daniel Krastev <danielkrastev01@gmail.com>
2026-03-24 11:44:45 +0200

    Set Loader in themes and posts

--- 4.Workshop-part-1/workshop/src/app/posts-list/posts-list.ts
+++ 4.Workshop-part-1/workshop/src/app/posts-list/posts-list.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ApiService } from '../apiService';
 import { Post } from '../types/post';
+import { Loader } from '../shared/loader/loader';
 
 @Component({
   selector: 'app-posts-list',
-  imports: [],
+  imports: [Loader],
   templateUrl: './posts-list.html',
   styleUrl: './posts-list.css',
 })
 export class PostsList implements OnInit {
   posts: Post[] = [];
-  constructor(private apiService: ApiService) {
+  isLoading = true;
+  constructor(private apiService: ApiService, private cd: ChangeDetectorRef) {
 
   }
 
@@ -18,6 +20,8 @@ export class PostsList implements OnInit {
     this.apiService.getPosts(5).subscribe(posts => {
       console.log(posts);
       this.posts = posts;
+      this.isLoading = false;
+      this.cd.detectChanges();
     })
   }
 }
--- 4.Workshop-part-1/workshop/src/app/themes-list/themes-list.ts
+++ 4.Workshop-part-1/workshop/src/app/themes-list/themes-list.ts
@@ -1,20 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ApiService } from '../apiService';
+import { Theme } from '../types/themes';
+import { Loader } from '../shared/loader/loader';
 
 @Component({
   selector: 'app-themes-list',
-  imports: [],
+  imports: [Loader],
   templateUrl: './themes-list.html',
   styleUrl: './themes-list.css',
 })
-export class ThemesList implements OnInit{
-  constructor(private apiService: ApiService) {
+export class ThemesList implements OnInit {
+  themes: Theme[] = [];
+  isLoading = true;
 
-  }
+  constructor(private apiService: ApiService, private cd: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     this.apiService.getThemes().subscribe(themes => {
-      console.log(themes);
+      this.themes = themes;
+      this.isLoading = false;
+      this.cd.detectChanges();
     });
   }
 }

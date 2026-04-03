c2f29865aa5eab03389ba1bf0e04e658267eb101
Daniel Krastev <danielkrastev01@gmail.com>
2026-04-03 23:03:34 +0300

    DUMMY Login and Logout

--- 5.Workshop-part-2/workshop/src/app/user/user.ts
+++ 5.Workshop-part-2/workshop/src/app/user/user.ts
@@ -1,10 +1,37 @@
 import { Injectable } from '@angular/core';
+import { UserForAuth } from '../types/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class User {
-  constructor() {}
+  USER_KEY = '[user]';
+  user: UserForAuth | null = null;
+  constructor() {
+    try {
 
-  //TODO: Save if the user is Authenticated in LocalStorage
+    } catch (error) {
+      //this.user = undefined;
+    }
+  }
+
+  login() {
+    this.user = {
+      firstName: 'Daniel',
+      email: 'daniel@abv.,bg',
+      phoneNumber: '123-123-123',
+      password: '123',
+      id: '1wdad2adsa2',
+    }
+    localStorage.setItem(this.USER_KEY, JSON.stringify(this.user));
+  }
+
+  register() {
+
+  }
+
+  logout() {
+    this.user = null;
+    localStorage.removeItem(this.USER_KEY);
+  }
 }

d36dd86868c5ec4263438063c0cf453cdc4baed3
Daniel Krastev <danielkrastev01@gmail.com>
2026-04-03 23:33:31 +0300

    Logout Welcome message

--- 5.Workshop-part-2/workshop/src/app/home/home.ts
+++ 5.Workshop-part-2/workshop/src/app/home/home.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { WelcomeMessage } from '../shared/welcome-message/welcome-message';
+import { UserService } from '../user/user';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [WelcomeMessage],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  get isLoggedIn(): boolean {
+    return this.userService.isLogged;
+  }
+  
+  constructor (private userService: UserService) {}
+}

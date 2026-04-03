cf48f4d6d666f456b1c01fff4092fa512152b54f
Daniel Krastev <danielkrastev01@gmail.com>
2026-04-03 21:37:17 +0300

    Routing Login and Register

--- 5.Workshop-part-2/workshop/src/app/app.routes.ts
+++ 5.Workshop-part-2/workshop/src/app/app.routes.ts
@@ -1,10 +1,18 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Error } from './error/error';
+import { Login } from './user/login/login';
+import { Register } from './user/register/register';
+import { Profile } from './user/profile/profile';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: Home },
+    // User routing
+    { path: 'login', component: Login },
+    { path: 'register', component: Register },
+    { path: 'profile', component: Profile },
+    ///
     { path: '404', component: Error },
     { path: '**', redirectTo: '/404' },
 ];
---
+++ 5.Workshop-part-2/workshop/src/app/user/user.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class User {
+  constructor() {}
+
+  //TODO: Save if the user is Authenticated in LocalStorage
+}

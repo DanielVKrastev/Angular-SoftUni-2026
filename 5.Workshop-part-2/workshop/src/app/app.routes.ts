c6310d4e1f2cf6753203a4644bdbaad74417272c
Daniel Krastev <danielkrastev01@gmail.com>
2026-04-03 21:39:01 +0300

    Routing Themes

--- 5.Workshop-part-2/workshop/src/app/app.routes.ts
+++ 5.Workshop-part-2/workshop/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { Error } from './error/error';
 import { Login } from './user/login/login';
 import { Register } from './user/register/register';
 import { Profile } from './user/profile/profile';
+import { ThemesList } from './theme/themes-list/themes-list';
+import { AddTheme } from './theme/add-theme/add-theme';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -13,6 +15,11 @@ export const routes: Routes = [
     { path: 'register', component: Register },
     { path: 'profile', component: Profile },
     ///
+    // Theme routing
+    { path: 'themes', component: ThemesList },
+    { path: 'add-theme', component: AddTheme },
+    //
+
     { path: '404', component: Error },
     { path: '**', redirectTo: '/404' },
 ];

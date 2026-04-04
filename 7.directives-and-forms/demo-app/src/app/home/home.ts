6d819ec4e984b4e50d71e07c592294a9bf24a1f6
Daniel Krastev <danielkrastev01@gmail.com>
2026-04-04 10:17:47 +0300

    Common Module Old way

--- 7.directives-and-forms/demo-app/src/app/home/home.ts
+++ 7.directives-and-forms/demo-app/src/app/home/home.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { HighLightDirective } from '../directives/highlight.directive';
 import { MyRouterLinkDirective } from '../directives/my-router-link.directive';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [HighLightDirective, MyRouterLinkDirective],
+  imports: [HighLightDirective, MyRouterLinkDirective, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  color = 'red';
+}

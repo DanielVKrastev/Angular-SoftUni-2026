4f182cd4d4c74673f7e095c02415bcbd04a8ecb0
Daniel Krastev <danielkrastev01@gmail.com>
2026-04-04 09:38:27 +0300

    Render Listen

--- 7.directives-and-forms/demo-app/src/app/directives/highlight.directive.ts
+++ 7.directives-and-forms/demo-app/src/app/directives/highlight.directive.ts
@@ -1,13 +1,42 @@
-import { Directive, OnInit } from "@angular/core";
+import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
 
 @Directive({
     selector: '[appHighLight]',
     standalone: true,
 })
-export class HighLightDirective implements OnInit{
+export class HighLightDirective implements OnInit {
+    constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
     ngOnInit(): void {
         console.log('Directive is loaded!');
-        
+        console.log(this.elRef.nativeElement); // = document.getElementById('para');
+
+        // ! Important: Bad practice
+        // ! this.elRef.nativeElement.style.background = 'red';
+
+        // Good Practice
+        //this.renderer.setStyle(this.elRef.nativeElement, 'background', 'pink');
+
+        this.renderer.listen(
+            this.elRef.nativeElement,
+            'mouseenter',
+            this.mouseEnterHandler.bind(this)
+        );
+
+        this.renderer.listen(
+            this.elRef.nativeElement, //element
+            'mouseleave',  //event
+            this.mouseLeaveHandler.bind(this) //function
+        );
+    }
+
+    mouseEnterHandler() {
+        console.log('mouse involked!');
+        this.renderer.setStyle(this.elRef.nativeElement, 'background', 'pink');
+    }
+
+    mouseLeaveHandler() {
+        console.log('mouse involked!');
+        this.renderer.setStyle(this.elRef.nativeElement, 'background', 'initial');
     }
 }

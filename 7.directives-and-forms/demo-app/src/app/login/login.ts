8de11e2bfa27d0b939ac11a24f482832a9c6a6a2
Daniel Krastev <danielkrastev01@gmail.com>
2026-04-04 11:14:57 +0300

    Login form

--- 7.directives-and-forms/demo-app/src/app/login/login.ts
+++ 7.directives-and-forms/demo-app/src/app/login/login.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
+  standalone: true,
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  @ViewChild('loginForm') form: NgForm | undefined;
+
+  formSubmitHandler() {
+
+    const form = this.form;
+
+    if (form?.invalid) {
+      console.log('this form is invalid!');
+      return;
+    }
+
+    console.log('Form has been submited');
+    console.log(form?.value); //controls
+
+  }
+}

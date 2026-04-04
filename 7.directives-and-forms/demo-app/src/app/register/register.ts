3fc2df5ea68faafd60ca44b5697bbb3180ba78fb
Daniel Krastev <danielkrastev01@gmail.com>
2026-04-04 11:59:35 +0300

    Register and validate

--- 7.directives-and-forms/demo-app/src/app/register/register.ts
+++ 7.directives-and-forms/demo-app/src/app/register/register.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {}
+export class Register {
+  registerForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.maxLength(5)]),
+  });
+
+  handleSubmit() {
+    if (this.registerForm.invalid) {
+      console.log('Form is invalid!');
+      return;
+    }
+
+    console.log(this.registerForm.value);
+
+    this.registerForm.reset();
+  }
+}

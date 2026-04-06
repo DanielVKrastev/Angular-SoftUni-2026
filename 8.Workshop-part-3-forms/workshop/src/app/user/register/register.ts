47da9f4be5b7564e015db89364f18bf58e67f826
Daniel Krastev <danielkrastev01@gmail.com>
2026-04-07 00:21:29 +0300

    Validator in template for email and username

--- 8.Workshop-part-3-forms/workshop/src/app/user/register/register.ts
+++ 8.Workshop-part-3-forms/workshop/src/app/user/register/register.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { emailValidator } from '../../utils/email.validator';
+import { EMAIL_DOMAINS } from '../../../constants';
 
 @Component({
   selector: 'app-register',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {}
+export class Register {
+  form = new FormGroup(
+    {
+      username: new FormControl('', [Validators.required, Validators.minLength(5)]),
+      email: new FormControl('', [Validators.required, emailValidator(EMAIL_DOMAINS)]),
+      tel: new FormControl(''),
+      // TODO: put password in Group
+      password: new FormControl('', [Validators.required]),
+      rePassword: new FormControl('', [Validators.required]),
+    }
+  );
+
+  isFieldTextMissing(controlName: string) {
+    return (
+      this.form.get(controlName)?.touched &&
+      this.form.get(controlName)?.errors?.['required']
+    );
+  }
+
+  get isNotMinLength() {
+    return (
+      this.form.get('username')?.touched &&
+      this.form.get('username')?.errors?.['minlength']
+    );
+  }
+
+  get isEmailNotValid() {
+    return (
+      this.form.get('email')?.touched &&
+      this.form.get('email')?.errors?.['emailValidator']
+    );
+  }
+
+  register(){
+    if(this.form.invalid){
+      return;
+    }
+    console.log(this.form.value);
+  }
+}

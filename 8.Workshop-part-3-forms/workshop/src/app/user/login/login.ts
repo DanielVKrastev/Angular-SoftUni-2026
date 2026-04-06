0800f773185733ac05169229eff70948d6ab89b0
Daniel Krastev <danielkrastev01@gmail.com>
2026-04-06 23:30:57 +0300

    Create Email Validator

--- 8.Workshop-part-3-forms/workshop/src/app/user/login/login.ts
+++ 8.Workshop-part-3-forms/workshop/src/app/user/login/login.ts
@@ -1,19 +1,27 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { UserService } from '../user';
+import { FormsModule, NgForm } from '@angular/forms';
+import { EmailDirective } from '../../../directives/email';
+import { EMAIL_DOMAINS } from '../../../constants';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule, EmailDirective],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  domains = EMAIL_DOMAINS;
+  
   constructor(private userService: UserService, private router: Router){}
 
-  login(event: Event, emailValue: string, passwordValue: string) {
-    event.preventDefault();
+  login(form: NgForm) {
+    if(form.invalid){
+      console.error('Invalid Login Form!');
+      return;
+    }
     
     this.userService.login();
     this.router.navigate(['/home']);

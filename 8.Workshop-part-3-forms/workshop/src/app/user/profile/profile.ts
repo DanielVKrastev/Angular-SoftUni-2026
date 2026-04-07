39e23aa083d21308b6147dbeb1e55c73f74a1538
Daniel Krastev <danielkrastev01@gmail.com>
2026-04-07 12:09:21 +0300

    Profile Updates

--- 8.Workshop-part-3-forms/workshop/src/app/user/profile/profile.ts
+++ 8.Workshop-part-3-forms/workshop/src/app/user/profile/profile.ts
@@ -1,9 +1,49 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { email } from '@angular/forms/signals';
+import { emailValidator } from '../../utils/email.validator';
+import { EMAIL_DOMAINS } from '../../../constants';
+import { ProfileDetails } from '../../types/user';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [ReactiveFormsModule],
+  standalone: true,
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
-export class Profile {}
+export class Profile {
+  isEditMode: boolean = false;
+
+  profileDetails:ProfileDetails = {
+    username: 'Daniel',
+    email: 'danielk@gmail.com',
+    tel: '123-123-123',
+  }
+
+  form = new FormGroup({
+    username: new FormControl(this.profileDetails.username, [Validators.required, Validators.minLength(5)]),
+    email: new FormControl(this.profileDetails.email, [Validators.required, emailValidator(EMAIL_DOMAINS)]),
+    tel: new FormControl(this.profileDetails.tel),
+  });
+
+  toggleEditMode() {
+    this.isEditMode = !this.isEditMode;
+  }
+
+  handleSaveProfile(){
+
+    if(this.form.invalid){
+      return;
+    }
+
+    this.profileDetails = this.form.value as ProfileDetails;
+  }
+
+  onCancel(event: Event) {
+    event.preventDefault();
+    this.form.updateValueAndValidity();
+    
+    this.toggleEditMode();
+  }
+}

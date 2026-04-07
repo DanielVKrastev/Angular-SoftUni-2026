23ac39259660209a168001c8bb7e69aa28c2756f
Daniel Krastev <danielkrastev01@gmail.com>
2026-04-07 11:10:05 +0300

    Add theme validation field

--- 8.Workshop-part-3-forms/workshop/src/app/theme/add-theme/add-theme.ts
+++ 8.Workshop-part-3-forms/workshop/src/app/theme/add-theme/add-theme.ts
@@ -1,21 +1,30 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../../apiService';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-theme',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './add-theme.html',
   styleUrl: './add-theme.css',
 })
 export class AddTheme {
   constructor(private apiService: ApiService){}
 
-  addTheme(event: Event, themeName: string, postText: string) {
-    event.preventDefault();
-    console.log(themeName, postText);
+  addTheme(form: NgForm) {
+    console.log(form);
+    
+    if(form.invalid){
+      return;
+    }
+    
+    console.log(form.value);
+    /*
     this.apiService.createTheme(themeName, postText).subscribe(data => {
       console.log(data);
       //TODO: send data to server
     })
+      */
   }
 }

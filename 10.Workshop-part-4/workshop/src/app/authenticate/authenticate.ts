ba0a4cdc637c604f370712f9b21ebc3fe44b2ae9
Daniel Krastev <danielkrastev01@gmail.com>
2026-04-10 00:01:27 +0300

    Authenticating and loader

--- 10.Workshop-part-4/workshop/src/app/authenticate/authenticate.ts
+++ 10.Workshop-part-4/workshop/src/app/authenticate/authenticate.ts
@@ -1,30 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { UserService } from '../user/user';
+import { Loader } from "../shared/loader/loader";
 
 @Component({
   selector: 'app-authenticate',
   standalone: true,
-  imports: [],
+  imports: [Loader],
   templateUrl: './authenticate.html',
   styleUrl: './authenticate.css',
 })
 export class AuthenticateComponent implements OnInit{
-  isAuth = true;
+  isAuthenticating = true;
 
-  constructor(private userService: UserService){}
+  constructor(private userService: UserService, private cd: ChangeDetectorRef){}
 
   ngOnInit(): void {
     this.userService.getProfile().subscribe({
       next: () => {
-        this.isAuth = false;
+        this.isAuthenticating = false;
+        this.cd.detectChanges();
       },
       error: () => {
-        this.isAuth = false;
+        this.isAuthenticating = false;
+        this.cd.detectChanges();
       },
       complete: () => {
-        this.isAuth = false;
-      }
+        this.isAuthenticating = false;
+        this.cd.detectChanges();
+      },
     });
+
   } 
 
 }

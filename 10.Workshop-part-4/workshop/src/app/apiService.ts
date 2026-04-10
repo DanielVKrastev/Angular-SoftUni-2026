f21d146565728184219f1ced73d6bd3e1e4d6b3d
Daniel Krastev <danielkrastev01@gmail.com>
2026-04-10 20:34:02 +0300

    Update apiservice delete and put operations

--- 10.Workshop-part-4/workshop/src/app/apiService.ts
+++ 10.Workshop-part-4/workshop/src/app/apiService.ts
@@ -43,11 +43,11 @@ export class ApiService {
   //Post theme - http.put
   updatePost(themeId:string, postId: string,) {
     const payload = { }
-    return this.http.put<Theme>(`/api/themes/${themeId}/posts:${postId}`, payload);
+    return this.http.put<Theme>(`/api/themes/${themeId}/posts/${postId}`, payload);
   }
 
   //Delete theme - http.delete theme ID
   deletePost(themeId:string, postId: string,) {
-    return this.http.delete<Theme>(`/api/themes/${themeId}/posts:${postId}`);
+    return this.http.delete<Theme>(`/api/themes/${themeId}/posts/${postId}`);
   }
 }

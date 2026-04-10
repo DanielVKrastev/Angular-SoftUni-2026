be17b336cf7422ebdd5f9fe0c8e773b1a5a15e9b
Daniel Krastev <danielkrastev01@gmail.com>
2026-04-10 20:39:50 +0300

    Lazy loading add-theme component

--- 10.Workshop-part-4/workshop/src/app/app.routes.ts
+++ 10.Workshop-part-4/workshop/src/app/app.routes.ts
@@ -21,9 +21,13 @@ export const routes: Routes = [
     // Theme routing
     { path: 'themes', children: [
         { path: '', component: MainComponent },
-        { path: ':themeId', component: CurrentTheme, canActivate: [AuthGuard]  },
+        { path: ':themeId', component: CurrentTheme, canActivate: [AuthGuard] },
     ] },
-    { path: 'add-theme', component: AddTheme, canActivate: [AuthGuard] },
+    { path: 'add-theme', 
+        //component: AddTheme, 
+        loadComponent: () => import('./theme/add-theme/add-theme').then((c) => c.AddTheme), //lazy loading
+        canActivate: [AuthGuard],
+     },
     //
     { path: 'error', component: ErrorMsgComponent },
     { path: '404', component: PageNotFoundComponent },
